4815971751af393576e47585cc94f364537f2cc8
Lilibeth Mejia <lilibethmejia@Lilibeths-Air.attlocal.net>
2023-06-30 17:55:39 -0500

    added fixed localStorage

--- script.js
+++ script.js
@@ -1,26 +1,20 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 var status = $(".time-block");
 var userInput = $(".description");
 var saveBtn = $(".saveBtn");
 $("#currentDay").text(dayjs().format("dddd, MMM D, YYYY"));
 
-// $(function () {
-function saveInfo() {
-  var retrieveText = JSON.parse(localStorage.getItem(savedTime));
-  console.log(retrieveText);
-  userInput.textContent = retrieveText;
-
-  var savedTime = $(this).parent().attr("data-number");
+$(function () {
+ $(".saveBtn").on("click", function () {
+  var savedTime = $(this).parent().attr("id");
   var textValue = $(this).siblings(".description").val();
   console.log(textValue);
 
-  localStorage.setItem(savedTime, JSON.stringify(textValue));
+  localStorage.setItem(savedTime, textValue) ;
 
-  changeColor();
-
-}
+  // var retrieveText= JSON.parse(localStorage.getItem(savedTime));
+  
+})
 
 function changeColor() {
   var currentTime = dayjs().hour();
@@ -30,9 +24,12 @@ function changeColor() {
     var compareTime = parseInt($(this).attr("id").split("-")[1]);
 
     if (compareTime < currentTime) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
       $(this).addClass("past");
     } else if (compareTime === currentTime) {
       $(this).removeClass("past");
+      $(this).removeClass("future");
       $(this).addClass("present");
     } else {
       $(this).removeClass("past");
@@ -42,24 +39,15 @@ function changeColor() {
   });
 }
 
-saveBtn.on("click", saveInfo);
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-
-
-
-// TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this
+changeColor();
+})
